5fe985a3ff8b6b73d25a9c5b13e28e12f1b3a9db
Steven Morimoto <smorimoto2284@gmail.com>
2020-05-18 14:08:23 -0500

    Complete with initial setup instructions.  Moving on to user stories instructions.

---
+++ SuperHero/Models/People.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperHero.Models
+{
+    public class People
+    {
+        [Key]
+        public string SuperHeroName { get; set; }
+        public string AlterEgo { get; set; }
+        public string PrimarySuperPower { get; set; }
+        public string SecondarySuperPower { get; set; }
+        public string CatchPhrase { get; set; }
+    }
+
+
+}
